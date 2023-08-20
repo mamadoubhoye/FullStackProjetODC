861e811c125be3c6736e051c4f1ccb79b06e8f9e
mamadoubhoye <bhoyemariotte@gmail.com>
2023-08-20 11:34:31 +0000

    message5

--- Server/index.js
+++ Server/index.js
@@ -11,8 +11,14 @@ const DB_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+// app.use(cors());
+app.use(cors({
+    origin: 'https://fullstackodc.onrender.com', // use your actual domain name (or localhost), using * is not recommended
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+    credentials: true
+}))
 
 app.use("/auth",authRoutes);
 app.use("/posts", postRoutes);
